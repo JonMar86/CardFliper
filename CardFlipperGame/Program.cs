6f672db5665fbc214770eea53af40df77330c66e
JonMar86 <66481955+JonMar86@users.noreply.github.com>
2020-06-06 19:37:38 -0400

    Simple game saving
    Thanks to Steven Sanderson and arivera for sharing

--- CardFlipperGame/Program.cs
+++ CardFlipperGame/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using BlazorDownloadFile;
+using BlazorInputFile;
 
 namespace CardFlipperGame
 {
@@ -18,6 +20,7 @@ namespace CardFlipperGame
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddBlazorDownloadFile();
 
             await builder.Build().RunAsync();
         }
---
+++ CardFlipperGame/Saving/GameState.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace CardFlipperGame.Saving
+{
+    [JsonObject]
+    public class GameState
+    {
+        [JsonProperty]
+        public int Seed { get; set; }
+
+        [JsonProperty]
+        public string[] MatchesFound { get; set; }
+    }
+}
