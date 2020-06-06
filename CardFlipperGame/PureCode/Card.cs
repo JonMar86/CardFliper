e451d91a5d3c2385cc8e422c83b43c92d6ebc764
JonMar86 <66481955+JonMar86@users.noreply.github.com>
2020-06-06 13:06:34 -0400

    Displaying a grid of cards
    Included Simple Grid CSS from https://simplegrid.io/

---
+++ CardFlipperGame/PureCode/Card.cs
@@ -0,0 +1,24 @@
+﻿namespace CardFlipperGame.PureCode
+{
+    public class Card
+    {
+        public const string DefaultText = "?";
+
+        public Card(string displayedSide)
+            : this(displayedSide, DefaultText)
+        {            
+        }
+
+        public Card(string displayedSide, string hiddenSide)
+        {
+            this.HiddenSide = hiddenSide;
+            this.DisplayedSide = displayedSide;
+        }
+
+        public bool IsHiding { get; set; } = true;
+
+        public string HiddenSide { get; set; }
+
+        public string DisplayedSide { get; set; }
+    }
+}
