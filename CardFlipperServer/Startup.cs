2cb1162ec5ae1a806ae56638144a4dabd30a2b9a
JonMar86 <66481955+JonMar86@users.noreply.github.com>
2020-06-08 15:41:54 -0400

    Replacing template files with real ones

--- CardFlipperServer/Startup.cs
+++ CardFlipperServer/Startup.cs
@@ -28,7 +28,6 @@ namespace CardFlipperServer
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
             services.AddSingleton<GameSaverService>();
         }
 
